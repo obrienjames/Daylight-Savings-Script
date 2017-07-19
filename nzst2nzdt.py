85f3b86fb747996c43c74785cef71b0f5f3b7744
James O'Brien <jobrien@linz.govt.nz>
2017-07-20 10:51:03 +1200

    User requested amendements
    Removed the need to specify the file extension for input files and
    tidied up the code to be more standardised (i.e. double quotes for
    strings etc).

--- nzst2nzdt.py
+++ nzst2nzdt.py
@@ -7,6 +7,10 @@
 #   1) The input file is based on a calander year and the December 31st value is > 2300
 #   2) The input file is based on a financial year and the last date entry is > 2300.
 # In both instances the value will be removed from the output file as it wouldn't be correct in the calander type.
+#
+#V.1: Release
+#V.1.1: Minor changes to conform to LINZ standards, removed need to specify file extension
+
 
 import sys
 import os
@@ -26,7 +30,7 @@ class FixedOffset(tzinfo):
         #NOTE: the last part is to remind about deprecated POSIX GMT+h timezones
         #  that have the opposite sign in the name;
         #  the corresponding numeric value is not used e.g., no minutes
-        self.__name = '<%+03d%02d>%+d' % (hours, minutes, -hours)
+        self.__name = "<%+03d%02d>%+d" % (hours, minutes, -hours)
     def utcoffset(self, dt=None):
         return self.__offset
     def tzname(self, dt=None):
@@ -34,26 +38,26 @@ class FixedOffset(tzinfo):
     def dst(self, dt=None):
         return timedelta(0)
     def __repr__(self):
-        return 'FixedOffset(%d)' % (self.utcoffset().total_seconds() / 60)
+        return "FixedOffset(%d)" % (self.utcoffset().total_seconds() / 60)
 
-tree = ET.parse(raw_input('Input file (include suffix):'))
+tree = ET.parse(raw_input("Input .xml filename (reads for initial conversion) :")+".xml")
 root = tree.getroot()
 
-xml = xml.dom.minidom.parse(raw_input('PrettyXML input file (include suffix):'))
+xml = xml.dom.minidom.parse(raw_input("Input .xml filename again (reads for correct formatting) :")+".xml")
 pretty_xml_as_strong = xml.toprettyxml()
 
 root.tag
 root.attrib
 
-nzst=pytz.timezone('Pacific/Auckland')
+nzst=pytz.timezone("Pacific/Auckland")
 dtformat="%Y %B %d %H %M"
 insertlist=[]
-for month in root.findall('.//Month'):
-    monthname,year=month.get('Name').split()
-    for day in month.findall('Day'):
-        dayno=day.get('Num')
-        for hilo in day.findall('HiLo'):
-            time=hilo.find('Time').text
+for month in root.findall(".//Month"):
+    monthname,year=month.get("Name").split()
+    for day in month.findall("Day"):
+        dayno=day.get("Num")
+        for hilo in day.findall("HiLo"):
+            time=hilo.find("Time").text
             hour=time[:2]
             min=time[2:]
             dt=datetime.strptime(" ".join((year,monthname,dayno,hour,min)),dtformat)
@@ -62,10 +66,10 @@ for month in root.findall('.//Month'):
             if str(dt) == str(dt2):
                 continue
             newtime=dt2.strftime("%H%M")
-            hilo.find('Time').text=newtime
-            if newtime.startswith('00'):
+            hilo.find("Time").text=newtime
+            if newtime.startswith("00"):
                 day.remove(hilo)
-                newmonth=dt2.strftime('%B %Y')
+                newmonth=dt2.strftime("%B %Y")
                 newday=str(dt2.day)
                 search=".//Month[@Name='"+newmonth+"']/Day[@Num='"+newday+"']"
                 newparent=root.find(search)
@@ -80,8 +84,8 @@ for newparent,hilo in insertlist:
 #replaces 'Local Std Time' with 'Local Std or Daylight Time'.
 for elem in root.getiterator():
     if elem.text:
-        elem.text = elem.text.replace('Local Std Time', 'Local Std or Daylight Time')
+        elem.text = elem.text.replace("Local Std Time", "Local Std or Daylight Time")
     if elem.tail:
-        elem.tail = elem.tail.replace('Local Std Time', 'Local Std or Daylight Time')
+        elem.tail = elem.tail.replace("Local Std Time", "Local Std or Daylight Time")
 
-tree.write(raw_input('Output file (include suffix):'))
+tree.write(raw_input("Output xml filename :")+".xml")
